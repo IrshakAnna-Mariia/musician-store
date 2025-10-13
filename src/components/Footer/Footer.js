a1e6cffb66de8e8fb1e2e1055026bb1cadac129c
IrshakAnna-Mariia <irshakam@gmail.com>
2025-10-13 18:33:16 +0300

    feat: footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,5 +1,10 @@
 const Footer = () => {
-  return <footer>Footer</footer>;
+  return (
+    <footer className="footer">
+      <p>© 2025 Musician's Hub. All rights reserved.</p>
+      <p>Located in the Great Lake area · Open Mon–Fri, 9am–5pm</p>
+    </footer>
+  );
 };
 
 export default Footer;
